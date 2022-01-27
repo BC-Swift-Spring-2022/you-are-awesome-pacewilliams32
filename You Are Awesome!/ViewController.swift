50a243f08341dec9068e6d6f1b0fd0b035de2755
Pace Williams <pacewilliams32@gmail.com>
2022-01-27 14:54:18 -0500

    Completed 2.9 -- String Interpolation and Changing Images

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,31 +12,41 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayImage: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let bombMessage = "You Are Da Bomb!"
-        
-        
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            messageLabel.textColor = UIColor.blue
-            displayImage.image = UIImage(named: "image1")
-        } else if messageLabel.text == greatMessage{
-            messageLabel.text = bombMessage
-            messageLabel.textColor = UIColor.magenta
-            displayImage.image = UIImage(named: "image2")
-        } else {
-            messageLabel.text = awesomeMessage
-            messageLabel.textColor = UIColor.red
-            displayImage.image = UIImage(named: "image0")
-        }
-        
-    }
+   @IBAction func messageButtonPressed(_ sender: UIButton) {
+       let imageName = "image\(imageNumber)"
+       displayImage.image = UIImage(named: imageName)
+       
+       imageNumber = imageNumber + 1
+       if imageNumber == 3 {
+           imageNumber = 0
+       }
+       
+            //        let awesomeMessage = "You Are Awesome!"
+            //        let greatMessage = "You Are Great!"
+            //        let bombMessage = "You Are Da Bomb!"
+            //
+            //
+            //        if messageLabel.text == awesomeMessage {
+            //            messageLabel.text = greatMessage
+            //            messageLabel.textColor = UIColor.blue
+            //            displayImage.image = UIImage(named: "image1")
+            //        } else if messageLabel.text == greatMessage{
+            //            messageLabel.text = bombMessage
+            //            messageLabel.textColor = UIColor.magenta
+            //            displayImage.image = UIImage(named: "image2")
+            //        } else {
+            //            messageLabel.text = awesomeMessage
+            //            messageLabel.textColor = UIColor.red
+            //            displayImage.image = UIImage(named: "image0")
+            //        }
+            //
+   }
 }
 
