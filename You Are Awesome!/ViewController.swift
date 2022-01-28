8f2769d05de17c6f498d1cfd3c87d293d69ce41c
Pace Williams <pacewilliams32@gmail.com>
2022-01-28 13:44:21 -0500

    Completed 2.10 -- Arrays

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,40 +13,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayImage: UIImageView!
     
     var imageNumber = 0
-    
+    var messageNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-   @IBAction func messageButtonPressed(_ sender: UIButton) {
-       let imageName = "image\(imageNumber)"
-       displayImage.image = UIImage(named: imageName)
-       
-       imageNumber = imageNumber + 1
-       if imageNumber == 3 {
-           imageNumber = 0
-       }
-       
-            //        let awesomeMessage = "You Are Awesome!"
-            //        let greatMessage = "You Are Great!"
-            //        let bombMessage = "You Are Da Bomb!"
-            //
-            //
-            //        if messageLabel.text == awesomeMessage {
-            //            messageLabel.text = greatMessage
-            //            messageLabel.textColor = UIColor.blue
-            //            displayImage.image = UIImage(named: "image1")
-            //        } else if messageLabel.text == greatMessage{
-            //            messageLabel.text = bombMessage
-            //            messageLabel.textColor = UIColor.magenta
-            //            displayImage.image = UIImage(named: "image2")
-            //        } else {
-            //            messageLabel.text = awesomeMessage
-            //            messageLabel.textColor = UIColor.red
-            //            displayImage.image = UIImage(named: "image0")
-            //        }
-            //
-   }
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let imageName = "image\(imageNumber)"
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+        var inspireMessages = [awesomeMessage, greatMessage, bombMessage]
+        displayImage.image = UIImage(named: imageName)
+        messageLabel.text = inspireMessages[messageNumber]
+        imageNumber = imageNumber + 1
+        if imageNumber == 3 {
+            imageNumber = 0
+        }
+        messageNumber += 1
+        if messageNumber == inspireMessages.count {
+            messageNumber = 0
+        }
+        //  let awesomeMessage = "You Are Awesome!"
+        //let greatMessage = "You Are Great!"
+        //let bombMessage = "You Are Da Bomb!"
+        
+        //
+        //
+        //        if messageLabel.text == awesomeMessage {
+        //            messageLabel.text = greatMessage
+        //            messageLabel.textColor = UIColor.blue
+        //            displayImage.image = UIImage(named: "image1")
+        //        } else if messageLabel.text == greatMessage{
+        //            messageLabel.text = bombMessage
+        //            messageLabel.textColor = UIColor.magenta
+        //            displayImage.image = UIImage(named: "image2")
+        //        } else {
+        //            messageLabel.text = awesomeMessage
+        //            messageLabel.textColor = UIColor.red
+        //            displayImage.image = UIImage(named: "image0")
+        //        }
+        //
+    }
 }
 
