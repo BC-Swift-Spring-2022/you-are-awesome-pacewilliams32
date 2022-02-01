68b002ac4f1ff3dbd966a5a14b25c7b5467ace8d
Pace Williams <pacewilliams32@gmail.com>
2022-02-01 14:33:43 -0500

    Completed 2.13 -- While and Repeat-While Loops

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,52 +12,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayImage: UIImageView!
     
-    var imageNumber = 0
-    var messageNumber = 0
+    var imageNumber = -1
+    var messageNumber = -1
+    let totalNumberOfImages = 4
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let imageName = "image\(Int.random(in: 0...2))"
+        
         let awesomeMessage = "You Are Awesome!"
         let greatMessage = "You Are Great!"
         let bombMessage = "You Are Da Bomb!"
+        let fantasticMessage = "You Are Fantastic!"
+        let fabulousMessage = "Fabulous? That's You!"
         
-        var inspireMessages = [awesomeMessage, greatMessage, bombMessage]
-        messageLabel.text = inspireMessages[Int.random(in: 0...inspireMessages.count-1)]
+        let inspireMessages = [awesomeMessage, greatMessage, bombMessage, fantasticMessage, fabulousMessage]
         
-        displayImage.image = UIImage(named: imageName)
-        // messageLabel.text = inspireMessages[messageNumber]
-        //        imageNumber = imageNumber + 1
-        //        if imageNumber == 3 {
-        //            imageNumber = 0
-        //        }
-        //       // messageNumber += 1
-        //        //if messageNumber == inspireMessages.count {
-        //          //  messageNumber = 0
-        //        }
-        //  let awesomeMessage = "You Are Awesome!"
-        //let greatMessage = "You Are Great!"
-        //let bombMessage = "You Are Da Bomb!"
+        var newMessageNumber : Int
         
-        //
-        //
-        //        if messageLabel.text == awesomeMessage {
-        //            messageLabel.text = greatMessage
-        //            messageLabel.textColor = UIColor.blue
-        //            displayImage.image = UIImage(named: "image1")
-        //        } else if messageLabel.text == greatMessage{
-        //            messageLabel.text = bombMessage
-        //            messageLabel.textColor = UIColor.magenta
-        //            displayImage.image = UIImage(named: "image2")
-        //        } else {
-        //            messageLabel.text = awesomeMessage
-        //            messageLabel.textColor = UIColor.red
-        //            displayImage.image = UIImage(named: "image0")
-        //        }
-        //
+        repeat {
+            newMessageNumber = Int.random(in: 0...inspireMessages.count-1)
+        } while messageNumber == newMessageNumber
+                messageNumber = newMessageNumber
+                messageLabel.text = inspireMessages[messageNumber]
+        
+        var newImageNumber : Int
+        repeat {
+            newImageNumber = Int.random(in: 0...totalNumberOfImages)
+        } while imageNumber == newImageNumber
+                imageNumber = newImageNumber
+                displayImage.image = UIImage(named: "image\(imageNumber)")
+                    
+                    
+                    
+                    
     }
     
     
