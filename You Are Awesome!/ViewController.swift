a10829008d193b970a8bbd97f4e7341a139dff73
Pace Williams <pacewilliams32@gmail.com>
2022-02-02 14:06:01 -0500

    Completed 2.16 -- Created functions for DRY coding

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -15,14 +15,41 @@ class ViewController: UIViewController {
     
     var imageNumber = -1
     var messageNumber = -1
-    let totalNumberOfImages = 4
+    var soundNumber = -1
+    let totalNumberOfImages = 5
+    let totalNumberOfSounds = 6
     var audioPlayer : AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    func playSound(soundName : String) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+               try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: \(error.localizedDescription) Could not read data from the file sound0")
+            }
+            
+        } else {
+            print("ERROR: Could not read data from the file \(soundName)")
+
+        }
+    }
+    func nonRepeatingRandom(originalNumber : Int, upperBounds : Int) -> Int {
+        var newNumber : Int
+        
+        repeat {
+            newNumber = Int.random(in: 0...upperBounds)
+        } while originalNumber == newNumber
+        return newNumber
+        
+    }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        
+        //Declares messages -- inspireMessages array
         
         let awesomeMessage = "You Are Awesome!"
         let greatMessage = "You Are Great!"
@@ -32,35 +59,20 @@ class ViewController: UIViewController {
         
         let inspireMessages = [awesomeMessage, greatMessage, bombMessage, fantasticMessage, fabulousMessage]
         
-        var newMessageNumber : Int
+        // Calls nonRepeatingRandom func to display random message
         
-        repeat {
-            newMessageNumber = Int.random(in: 0...inspireMessages.count-1)
-        } while messageNumber == newMessageNumber
-                messageNumber = newMessageNumber
-                messageLabel.text = inspireMessages[messageNumber]
+        messageNumber = nonRepeatingRandom(originalNumber: messageNumber, upperBounds: inspireMessages.count-1)
+        messageLabel.text = inspireMessages[messageNumber]
         
-        var newImageNumber : Int
-        repeat {
-            newImageNumber = Int.random(in: 0...totalNumberOfImages)
-        } while imageNumber == newImageNumber
-                imageNumber = newImageNumber
-                displayImage.image = UIImage(named: "image\(imageNumber)")
-                    
-        if let sound = NSDataAsset(name: "sound0") {
-            do {
-               try audioPlayer = AVAudioPlayer(data: sound.data)
-                audioPlayer.play()
-            } catch {
-                print("ERROR: \(error.localizedDescription) Could not read data from the file sound0")
-            }
-            
-        } else {
-            print("ERROR: Could not read data from the file sound0")
-
-        }
-                    
-                    
+       // Displays random image
+        imageNumber = nonRepeatingRandom(originalNumber: imageNumber, upperBounds: totalNumberOfImages-1)
+        displayImage.image = UIImage(named: "image\(imageNumber)")
+        
+        // Plays random sound
+        soundNumber = nonRepeatingRandom(originalNumber: soundNumber, upperBounds: totalNumberOfSounds-1)
+        
+        playSound(soundName: "sound\(soundNumber)")
+        
     }
     
     
